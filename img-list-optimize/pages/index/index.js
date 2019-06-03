2836dfe343da803fa057e8db0eb57661027da8e1
timshen <timshen@tencent.com>
2019-06-03 17:43:18 +0800

    添加图片列表性能优化代码框架

--- img-list-optimize/pages/index/index.js
+++ img-list-optimize/pages/index/index.js
@@ -7,48 +7,36 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    itemList: [],
+    imgList: []
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    for (let i = 0; i < 10; i++) {
+      this.data.imgList.push({
+        url: '/imgs/octocat.png'
+      });
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+
+    for (let i = 0; i < 10; i++) {
+      this.data.itemList.push(1);
+    }
+
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      itemList: this.data.itemList,
+      imgList: this.data.imgList
+    })
+  },
+  onReachBottom() {
+    console.log(`reach bottom`);
+    this.appendItem(100);
+  },
+  appendItem(count) {
+    for (let i = 0; i < count; i++) {
+      this.data.itemList.push({});
+    }
+    this.setData({
+      itemList: this.data.itemList
     })
   }
-})
+});
