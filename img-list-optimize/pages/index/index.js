81401f3bb61ec3c9e2cd449bb32eb4cc456771d6
ssthouse <ssthouse@163.com>
2019-06-08 13:57:32 +0800

    完成列表优化demo

--- img-list-optimize/pages/index/index.js
+++ img-list-optimize/pages/index/index.js
@@ -1,34 +1,23 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     itemList: [],
-    imgList: []
   },
   onLoad: function () {
-    for (let i = 0; i < 10; i++) {
-      this.data.imgList.push({
-        url: '/imgs/octocat.png'
-      });
-    }
-
-    for (let i = 0; i < 10; i++) {
-      this.data.itemList.push(1);
-    }
-
+    this.initItemList();
     this.setData({
       itemList: this.data.itemList,
       imgList: this.data.imgList
     })
   },
+  initItemList() {
+    for (let i = 0; i < 10; i++) {
+      this.data.itemList.push({
+        url: `/imgs/octocat.png`,
+        content: `第 ${i + 1} 个元素`
+      });
+    }
+  },
   onReachBottom() {
-    console.log(`reach bottom`);
     this.appendItem(100);
   },
   appendItem(count) {
