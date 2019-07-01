c3a741123715fa91e5bc6b0de2287ea4711bed4c
ssthouse <ssthouse@163.com>
2019-07-01 21:56:35 +0800

    chapter_2: 添加webpack依赖

---
+++ chapter_2/README.md
@@ -0,0 +1,7 @@
+## 安装webpack依赖
+
+>     "clean-webpack-plugin": "^3.0.0",
+      "html-webpack-plugin": "^3.2.0",
+      "webpack": "^4.35.2",
+      "webpack-cli": "^3.3.5",
+      "webpack-dev-server": "^3.7.2"
--- chapter_2/webpack.config.js
+++ chapter_2/webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.js',
   output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
