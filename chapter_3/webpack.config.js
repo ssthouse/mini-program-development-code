9aab5eae880817f7c8451f297a1fcf9333d4c369
ssthouse <ssthouse@163.com>
2019-09-08 10:34:16 +0800

    chapter_3: 初始化汇率计算器项目

---
+++ chapter_3/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const babel = require('@babel/core')
+const less = require('less')
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: '**/*.wxml',
+        to: './'
+      },
+      {
+        from: '**/*.json',
+        to: './'
+      },
+      {
+        from: '**/*.jpg',
+        to: './'
+      },
+      {
+        from: '**/*.png',
+        to: './'
+      },
+      {
+        from: '**/*.css',
+        to: './'
+      },
+      {
+        from: '**/*.less',
+        to: './',
+        transform(content, path) {
+          return less.render(content.toString())
+            .then(function (output) {
+              return output.css;
+            });
+        }
+      },
+      {
+        from: '**/*.js',
+        ignore: ['*.test.js', '*.spec.js'],
+        to: './',
+        transform(content, path) {
+          const newCode = babel.transformSync(content, {
+            babelrc: true,
+            "presets": ["@babel/env"]
+          }).code;
+          return Promise.resolve(newCode.toString());
+        },
+      },
+    ],{
+      context: './src'
+    })
+  ]
+}
