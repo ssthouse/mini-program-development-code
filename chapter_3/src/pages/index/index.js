eb5f70a3064678f754a2c9692bbdd04eea02b024
ssthouse <ssthouse@163.com>
2019-09-08 11:26:49 +0800

    chapter3: 完成基本数据展示

--- chapter_3/src/pages/index/index.js
+++ chapter_3/src/pages/index/index.js
@@ -1,9 +1,31 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const exchangeRateMap = require('../../utils/exchange-rate')
 
 Page({
   data: {
     motto: 'Hello World',
+    exchangeList: [],
+    baseMoneyKey: "CNY"
   },
+  onLoad () {
+    // 初始化列表数据
+    delete exchangeRateMap.CNY
+    const exchangeRateList = []
+    for (let moneyKey of Object.keys(exchangeRateMap)) {
+      console.log(moneyKey)
+      exchangeRateList.push({
+        key: moneyKey,
+        ...exchangeRateMap[moneyKey]
+      })
+    }
+    console.log(exchangeRateList)
+    this.setData({
+      'exchangeList': exchangeRateList
+    })
+  },
+  onBaseNumChange (event) {
+    console.log(event.detail.value)
+  }
 })
---
+++ chapter_3/src/utils/exchange-rate.js
@@ -0,0 +1,22 @@
+module.exports = {
+  CNY: {
+    baseNum: 100,
+    name: '人民币 ¥'
+  },
+  USD: {
+    baseNum: 14.05,
+    name: '美元 $'
+  },
+  JPY: {
+    baseNum: 1502.31,
+    name: '日元 ￥'
+  },
+  EUR: {
+    baseNum: 12.74,
+    name: '欧元 €'
+  },
+  HKD: {
+    baseNum: 110.17,
+    name: '港币 $'
+  }
+}
