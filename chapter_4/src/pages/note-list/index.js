b2f7f398502ad9f3bd3a9e38d485df3d40629142
ssthouse <ssthouse@163.com>
2019-10-07 16:57:03 +0800

    chapter4: 完成页面基本跳转逻辑

--- chapter_4/src/pages/note-list/index.js
+++ chapter_4/src/pages/note-list/index.js
@@ -5,9 +5,29 @@ const app = getApp()
 Page({
   data: {
     noteList: [
-      {content: 'note1'},
-      {content: 'note2'},
-      {content: 'note3'}
+      {
+        id: 'note1',
+        content: 'note1'
+      },
+      {
+        id: 'note2',
+        content: 'note2'
+      },
+      {
+        id: 'note3',
+        content: 'note3'
+      }
     ]
   },
+  onCreateNote () {
+    wx.navigateTo({
+      url: `/pages/note/index?create=true`
+    })
+  },
+  onClickNote (event) {
+    const noteId = event.currentTarget.dataset['noteId']
+    wx.navigateTo({
+      url: `/pages/note/index?noteId=${noteId}`
+    })
+  }
 })
