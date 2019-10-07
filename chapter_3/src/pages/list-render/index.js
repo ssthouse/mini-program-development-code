2f68b150e78f14d84cf33b2bd695c209a5f761e9
ssthouse <ssthouse@163.com>
2019-10-07 10:52:44 +0800

    chapter3: 完成列表渲染demo

--- chapter_3/src/pages/list-render/index.js
+++ chapter_3/src/pages/list-render/index.js
@@ -1,9 +1,30 @@
 Page({
   data: {
     bookList: [
-      {name: 'Java编程思想'},
-      {name: '重构改善既有代码质量'},
-      {name: '代码整洁之道'}
+      {
+        name: 'Java编程思想',
+        priceList: [
+          {name: '京东', price: 42},
+          {name: '淘宝', price: 41},
+          {name: '当当', price: 43}
+        ]
+      },
+      {
+        name: '重构改善既有代码质量',
+        priceList: [
+          {name: '京东', price: 51.5},
+          {name: '淘宝', price: 52},
+          {name: '当当', price: 51}
+        ]
+      },
+      {
+        name: '代码整洁之道',
+        priceList: [
+          {name: '京东', price: 58},
+          {name: '淘宝', price: 56},
+          {name: '当当', price: 57}
+        ]
+      }
     ]
-  },
+  }
 })
