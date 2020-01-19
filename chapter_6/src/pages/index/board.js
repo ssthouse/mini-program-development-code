c3ee2ed6f3cf13612cb17d0f7f8aeed2548d7b7e
ssthouse <ssthouse@163.com>
2020-01-19 15:10:00 +0800

    chapter6: 实现基本UI布局

---
+++ chapter_6/src/pages/index/board.js
@@ -0,0 +1,6 @@
+class Board{
+
+  constructor(size) {
+    this.size = size
+  }
+}
--- chapter_6/src/pages/index/index.js
+++ chapter_6/src/pages/index/index.js
@@ -5,5 +5,17 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    // 棋盘
+    matrix: [
+      [1,2,3,4],
+      [1,2,3,4],
+      [1,2,3,4],
+      [1,2,3,4],
+    ],
+    highestScore: 0,
+    currentScore: 0,
   },
+  startGame() {
+    console.log('start game')
+  }
 })
