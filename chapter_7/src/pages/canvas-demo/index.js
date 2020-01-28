b259f4d206d609e8d7d408c165411ecf345cedad
ssthouse <ssthouse@163.com>
2020-01-28 20:28:48 +0800

    chapter7: 搭建canvas playground

--- chapter_7/src/pages/canvas-demo/index.js
+++ chapter_7/src/pages/canvas-demo/index.js
@@ -4,6 +4,14 @@ Page({
     logs: []
   },
   onLoad: function () {
-
+    this.drawReact()
+  },
+  drawReact() {
+    const context = wx.createCanvasContext('canvas');
+    context.setStrokeStyle("#000000")
+    context.setLineWidth(1)
+    context.rect(0, 0, 200, 200)
+    context.stroke()
+    context.draw()
   }
 })
