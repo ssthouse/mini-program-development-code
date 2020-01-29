063b76ec37de59ea4bc6cb6dd1520dcaba52b8bf
ssthouse <ssthouse@163.com>
2020-01-29 12:20:54 +0800

    chapter7: 调整UI布局

--- chapter_7/src/pages/canvas-demo/index.js
+++ chapter_7/src/pages/canvas-demo/index.js
@@ -1,13 +1,36 @@
+const regeneratorRuntime = require('../../lib/runtime') // eslint-disable-line
+
+const CANVAS_ID = 'canvas'
 
 Page({
   data: {
     logs: []
   },
   onLoad: function () {
-    this.drawReact()
   },
-  drawReact() {
-    const context = wx.createCanvasContext('canvas');
+  getCanvasContext() {
+    return wx.createCanvasContext(CANVAS_ID, this);
+  },
+  /**
+   * 获取画布大小
+   * @return {Promise<number>}
+   */
+  async getCanvasSize() {
+    return new Promise((resolve, reject) => {
+      wx.createSelectorQuery().select('.canvas')
+        .boundingClientRect(function (rect) {
+          resolve(rect['width'])
+        }).exec()
+    })
+  },
+  async onClearCanvas() {
+    const canvasSize = await this.getCanvasSize()
+    const context = this.getCanvasContext()
+    context.clearRect(0, 0, canvasSize, canvasSize)
+    context.draw()
+  },
+  onDrawReact() {
+    const context = this.getCanvasContext()
     context.setStrokeStyle("#000000")
     context.setLineWidth(1)
     context.rect(0, 0, 200, 200)
