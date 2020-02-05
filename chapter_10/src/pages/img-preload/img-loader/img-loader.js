3842b0ccca026bd3f1d048c618cdb445628b6e3f
ssthouse <ssthouse@163.com>
2020-02-05 16:48:21 +0800

    chapter10: 优化img-loader实现

--- chapter_10/src/pages/img-preload/img-loader/img-loader.js
+++ chapter_10/src/pages/img-preload/img-loader/img-loader.js
@@ -2,24 +2,17 @@ Component({
   properties: {},
   data: {
     imgSrc: '',
-    requestQueue: [],
-    isLoading: false
+    requestQueue: []
   },
   methods: {
-    // public api begin
     loadImg(url, callback) {
       this.data.requestQueue.push({
         url,
         callback
       });
-      this.triggerLoadingImg();
-    },
-    // public api end
-
-    // private methods begin
-    triggerLoadingImg() {
-      if (this.data.isLoading) return;
-      this.loadNextImg();
+      if (this.data.requestQueue.length === 1) {
+        this.loadNextImg()
+      }
     },
     onImageLoaded(detail) {
       console.log(detail);
@@ -29,13 +22,11 @@ Component({
     },
     loadNextImg() {
       if (this.data.requestQueue.length === 0) {
-        this.data.isLoading = false;
         return;
       }
       this.setData({
         imgSrc: this.data.requestQueue[0].url
       })
     }
-    // private methods end
   }
 });
