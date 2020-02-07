a39edfc1e1b385591c8285319b1a56b913a8851b
ssthouse <ssthouse@163.com>
2020-02-07 14:43:15 +0800

    chapter11: 增加jest demo

---
+++ chapter_11/src/pages/unit-test/board.js
@@ -0,0 +1,203 @@
+const MATRIX_SIZE = 4
+
+const MOVE_DIRECTION = {
+  LEFT: 0,
+  TOP: 1,
+  RIGHT: 2,
+  BOTTOM: 3
+}
+
+class Point {
+  constructor(rowIndex, columnIndex) {
+    this.rowIndex = rowIndex
+    this.columnIndex = columnIndex
+  }
+}
+
+module.exports.MOVE_DIRECTION = MOVE_DIRECTION
+
+function printMatrix(matrix) {
+  for (let row of matrix) {
+    console.log(row.join(' '))
+  }
+}
+
+class Board {
+
+  constructor() {
+    this.matrix = []
+    this.currentScore = 0
+    this.fillEmptyMatrix()
+  }
+
+  fillEmptyMatrix() {
+    for (let i = 0; i < MATRIX_SIZE; i++) {
+      const row = []
+      for (let j = 0; j < MATRIX_SIZE; j++) {
+        row.push(0)
+      }
+      this.matrix.push(row)
+    }
+  }
+
+  randomIndex() {
+    return Math.floor(Math.random() * MATRIX_SIZE)
+  }
+
+  startGame() {
+    // 初始化两个cell
+    for (let i = 0; i < 2; i++) {
+      this.matrix[this.randomIndex()][this.randomIndex()] = Math.random() < 0.8 ? 2 : 4
+    }
+  }
+
+  moveValidNumToLeft(matrix) {
+    const movedMatrix = []
+    for (let i = 0; i < MATRIX_SIZE; i++) {
+      const row = []
+      for (let j = 0; j < MATRIX_SIZE; j++) {
+        if (matrix[i][j] !== 0) {
+          row.push(matrix[i][j])
+        }
+      }
+      while (row.length < MATRIX_SIZE) {
+        row.push(0)
+      }
+      movedMatrix.push(row)
+    }
+    return movedMatrix
+  }
+
+  move(direction) {
+    if (!this.canMove(direction)) {
+      console.log('该方向不可用')
+      return
+    }
+    const rotatedMatrix = this.transformMatrixToDirectionLeft(this.matrix, direction)
+    const leftMovedMatrix = this.moveValidNumToLeft(rotatedMatrix)
+    // 相同数字合并
+    for (let i = 0; i < MATRIX_SIZE; i++) {
+      for (let j = 0; j < MATRIX_SIZE - 1; j++) {
+        if (leftMovedMatrix[i][j] > 0 && leftMovedMatrix[i][j] === leftMovedMatrix[i][j + 1]) {
+          leftMovedMatrix[i][j] *= 2;
+          this.currentScore += leftMovedMatrix[i][j];
+          leftMovedMatrix[i][j + 1] = 0;
+        }
+      }
+    }
+    const againMovedMatrix = this.moveValidNumToLeft(leftMovedMatrix)
+    this.matrix = this.reverseTransformMatrixFromDirectionLeft(againMovedMatrix, direction)
+
+    // 增加一个新数字
+    const emptyPoints = this.getEmptyCells();
+    if (emptyPoints.length !== 0) {
+      const emptyPoint = emptyPoints[Math.floor(Math.random() * emptyPoints.length)]
+      this.matrix[emptyPoint.rowIndex][emptyPoint.columnIndex] = Math.random() < 0.8 ? 2 : 4
+    }
+  }
+
+  transformMatrixToDirectionLeft(matrix, direction) {
+    switch (direction) {
+      case MOVE_DIRECTION.LEFT:
+        return matrix
+      case MOVE_DIRECTION.TOP:
+        return this.rotateMultipleTimes(matrix, 3);
+      case MOVE_DIRECTION.RIGHT:
+        return this.rotateMultipleTimes(matrix, 2);
+      case MOVE_DIRECTION.BOTTOM:
+        return this.rotateMatrix(matrix);
+      default:
+        return matrix
+    }
+  }
+
+  reverseTransformMatrixFromDirectionLeft(matrix, direction) {
+    switch (direction) {
+      case MOVE_DIRECTION.LEFT:
+        return matrix
+      case MOVE_DIRECTION.TOP:
+        return this.rotateMultipleTimes(matrix, 1);
+      case MOVE_DIRECTION.RIGHT:
+        return this.rotateMultipleTimes(matrix, 2);
+      case MOVE_DIRECTION.BOTTOM:
+        return this.rotateMultipleTimes(matrix, 3);
+      default:
+        return matrix
+    }
+  }
+
+  rotateMultipleTimes(matrix, rotateNum) {
+    let newMatrix = matrix
+    while (rotateNum > 0) {
+      newMatrix = this.rotateMatrix(newMatrix)
+      rotateNum--
+    }
+    return newMatrix
+  }
+
+  // 顺时针旋转90°
+  rotateMatrix(matrix) {
+    const rotatedMatrix = []
+    for (let i = 0; i < MATRIX_SIZE; i++) {
+      const row = []
+      for (let j = MATRIX_SIZE - 1; j >= 0; j--) {
+        row.push(matrix[j][i])
+      }
+      rotatedMatrix.push(row)
+    }
+    return rotatedMatrix
+  }
+
+  canMove(direction) {
+    const rotatedMatrix = this.transformMatrixToDirectionLeft(this.matrix, direction)
+    // 根据direction, 改为向左判断
+    for (let i = 0; i < MATRIX_SIZE; i++) {
+      for (let j = 0; j < MATRIX_SIZE; j++) {
+        // 如果有两个连着相等的,可以滑动
+        if (rotatedMatrix[i][j] > 0 && rotatedMatrix[i][j] === rotatedMatrix[i][j + 1]) {
+          return true;
+        }
+        // 如果有数字左边有0,可以滑动
+        if (rotatedMatrix[i][j] === 0 && rotatedMatrix[i][j + 1] > 0) {
+          return true;
+        }
+      }
+    }
+    return false
+  }
+
+  isGameOver() {
+    return !this.canMove(MOVE_DIRECTION.LEFT) &&
+      !this.canMove(MOVE_DIRECTION.TOP) &&
+      !this.canMove(MOVE_DIRECTION.RIGHT) &&
+      !this.canMove(MOVE_DIRECTION.BOTTOM)
+  }
+
+  getEmptyCells() {
+    const emptyCells = []
+    for (let i = 0; i < MATRIX_SIZE; i++) {
+      for (let j = 0; j < MATRIX_SIZE; j++) {
+        if (this.matrix[i][j] === 0) {
+          emptyCells.push(new Point(i, j))
+        }
+      }
+    }
+    return emptyCells
+  }
+
+  isWinning() {
+    let max = 0
+    const winNum = 2048
+    for (let row of this.matrix) {
+      for (let cell of row) {
+        max = Math.max(cell, max)
+      }
+      if (max > winNum) {
+        return false
+      }
+    }
+    return max === winNum
+  }
+}
+
+module.exports.Board = Board
---
+++ chapter_11/src/pages/unit-test/board.test.js
@@ -0,0 +1,33 @@
+const board = require('./board')
+
+const testBoard = new board.Board()
+const originMatrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+]
+
+const targetMatrix = [
+  [13, 9, 5, 1],
+  [14, 10, 6, 2],
+  [15, 11, 7, 3],
+  [16, 12, 8, 4]
+]
+
+function isSameMatrix(matrixA, matrixB) {
+  for (let i = 0; i < matrixA.length; i++) {
+    for (let j = 0; j < matrixA[0].length; j++) {
+      if (matrixA[i][j] !== matrixB[i][j]) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
+// console.assert(isSameMatrix(targetMatrix, testBoard.rotateMatrix(originMatrix)), '二维数组旋转逻辑有误')
+
+test('rotated matrix should be same', () => {
+  expect(isSameMatrix(targetMatrix, testBoard.rotateMatrix(originMatrix))).toBe(true)
+})
