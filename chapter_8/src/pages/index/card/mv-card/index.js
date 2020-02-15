a116fe33f689de7e3bdc2f8679692dcb8b4d2339
ssthouse <ssthouse@163.com>
2020-02-15 20:05:46 +0800

    chapter8: 增加MV推荐列表

---
+++ chapter_8/src/pages/index/card/mv-card/index.js
@@ -0,0 +1,31 @@
+/**
+ * mv bean:
+ * {
+    "id": 10915413,
+    "type": 5,
+    "name": "Cry Cry Cry",
+    "copywriter": "编辑推荐：Coldplay新单音乐录像带特别放送！",
+    "picUrl": "http://p1.music.126.net/91YsyhuIRJzoW6YLYlJYcg==/109951164707027248.jpg",
+    "canDislike": false,
+    "trackNumberUpdateTime": null,
+    "duration": 214000,
+    "playCount": 21284,
+    "subed": false,
+    "artists": [{
+        "id": 89365,
+        "name": "Coldplay"
+    }],
+    "artistName": "Coldplay",
+    "artistId": 89365,
+    "alg": "featured"
+}
+ */
+
+Component({
+  properties: {
+    mv: {
+      type: Object,
+      value: {}
+    }
+  }
+})
