853dc8b6e4df88075338553fb28f636ff20be9fb
ssthouse <ssthouse@163.com>
2020-02-15 19:37:57 +0800

    chapter8: 抽取歌单卡片组件

---
+++ chapter_8/src/pages/index/card/playlist-card/index.js
@@ -0,0 +1,32 @@
+Component({
+  properties: {
+    playlist: {
+      type: Object,
+      value: {
+        alg: "featured",
+        canDislike: false,
+        copywriter: "编辑推荐：《歌手·当打之年》最新音源收录！",
+        highQuality: false,
+        id: 3233380300,
+        name: "歌手·当打之年1-2期现场及原曲合集",
+        picUrl: "http://p1.music.126.net/GMy_E4iX4_IVWGHMw5bwZw==/109951164685568806.jpg",
+        playCount: 7994619,
+        trackCount: 32,
+        trackNumberUpdateTime: 1581729072582,
+        type: 0,
+      }
+    }
+  },
+  data: {
+    dateNumber: 0,
+  },
+  methods: {},
+  lifetimes: {
+    attached() {
+      const today = new Date()
+      this.setData({
+        dateNumber: today.getDate()
+      })
+    }
+  }
+})
