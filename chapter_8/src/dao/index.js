ee2bef5867a33710b497c3a9eadf7653272b1b05
ssthouse <ssthouse@163.com>
2020-02-15 15:41:07 +0800

    chapter8: 增加获取首页数据接口

---
+++ chapter_8/src/dao/index.js
@@ -0,0 +1,60 @@
+const regeneratorRuntime = require('../lib/runtime'); // eslint-disable-line
+
+const BASE_URL = `http://localhost:3000/v1/`
+
+async function getRecommendPlaylist() {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: BASE_URL + 'personalized',
+      data: {cookie: ''},
+      success: function (res) {
+        resolve(res.data.result)
+      }
+    })
+  })
+}
+
+//  'personalized/newsong', 'personalized/mv', 'personalized/djprogram'
+
+async function getRecommendNewSong() {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: BASE_URL + 'personalized/newsong',
+      data: {cookie: ''},
+      success: function (res) {
+        resolve(res.data.result)
+      }
+    })
+  })
+}
+
+async function getRecommendMV() {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: BASE_URL + 'personalized/mv',
+      data: {cookie: ''},
+      success: function (res) {
+        resolve(res.data.result)
+      }
+    })
+  })
+}
+
+async function getRecommendDJ() {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: BASE_URL + 'personalized/djprogram',
+      data: {cookie: ''},
+      success: function (res) {
+        resolve(res.data.result)
+      }
+    })
+  })
+}
+
+module.exports = {
+  getRecommendPlaylist,
+  getRecommendNewSong,
+  getRecommendMV,
+  getRecommendDJ
+}
--- chapter_8/src/pages/index/index.js
+++ chapter_8/src/pages/index/index.js
@@ -1,5 +1,39 @@
+const regeneratorRuntime = require('../../lib/runtime'); // eslint-disable-line
+
+const dao = require('../../dao/index')
+
 Page({
-  data: {},
-  onLoad: function (options) {
+  data: {
+    loading: true,
+    playList: [],
+    newSongList: [],
+    mvList: [],
+    djList: [],
   },
+  onLoad() {
+    this.initData()
+  },
+  async initData() {
+    try {
+      const [playList, newSongList, mvList, djList] = await Promise.all([
+        dao.getRecommendPlaylist(),
+        dao.getRecommendNewSong(),
+        dao.getRecommendMV(),
+        dao.getRecommendDJ()
+      ])
+      this.setData({
+        loading: false,
+        playList,
+        newSongList,
+        mvList,
+        djList,
+      })
+    } catch (e) {
+      wx.showToast({
+        icon: 'none',
+        duration: 1500,
+        title: '获取数据失败, 请稍后重试'
+      });
+    }
+  }
 })
