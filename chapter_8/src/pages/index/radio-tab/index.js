ed4ac4a42b529fddd6c9e5ae8dc6f07f08203210
ssthouse <ssthouse@163.com>
2020-03-03 22:52:27 +0800

    chapter8: 增加radio tab页

---
+++ chapter_8/src/pages/index/radio-tab/index.js
@@ -0,0 +1,71 @@
+const regeneratorRuntime = require('../../../lib/runtime'); // eslint-disable-line
+const dao = require('../../../dao/index')
+
+const LIMIT = 20
+
+Component({
+  data: {
+    hasMore: true,
+    radioList: [],
+    showLoading: false,
+    offset: 0,
+  },
+  lifetimes: {
+    attached() {
+      this.fetchRecommendPrograms()
+      this.fetchRecommendRadios()
+    }
+  },
+  methods: {
+    async fetchRecommendPrograms() {
+      this.setData({
+        showLoading: true
+      })
+      try {
+        const response = await dao.getPlaylistByCategory(this.data.selectedCategory, this.data.offset, LIMIT)
+        const newPlaylists = response.playlists
+        const newData = {
+          hasMore: response['more'],
+        }
+        if (newPlaylists) {
+          newData.playlists = this.data.playlists.concat(newPlaylists)
+          this.data.offset = newData.playlists.length
+        }
+        this.setData(newData)
+      } catch (e) {
+        wx.showToast({
+          duration: 2000,
+          title: '获取歌单信息失败'
+        })
+      }
+      this.setData({
+        showLoading: false
+      })
+    },
+    async fetchRecommendRadios() {
+      this.setData({
+        showLoading: true
+      })
+      try {
+        const response = await dao.getPlaylistByCategory(this.data.selectedCategory, this.data.offset, LIMIT)
+        const newPlaylists = response.playlists
+        const newData = {
+          hasMore: response['more'],
+        }
+        if (newPlaylists) {
+          newData.playlists = this.data.playlists.concat(newPlaylists)
+          this.data.offset = newData.playlists.length
+        }
+        this.setData(newData)
+      } catch (e) {
+        wx.showToast({
+          duration: 2000,
+          title: '获取歌单信息失败'
+        })
+      }
+      this.setData({
+        showLoading: false
+      })
+    },
+  },
+})
