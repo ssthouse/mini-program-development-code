2de3f655fbdafd5c70730c310c085ad960586fb7
ssthouse <ssthouse@163.com>
2020-03-06 10:38:50 +0800

    chapter8: 实现基本radio页面播放逻辑

--- chapter_8/src/pages/radio/play/index.js
+++ chapter_8/src/pages/radio/play/index.js
@@ -1,12 +1,11 @@
 const regeneratorRuntime = require('../../../lib/runtime'); // eslint-disable-line
 const dao = require('../../../dao/index')
-
-const PAGE_SIZE = 20
+const playerManager = require('../../../utils/player-manager')
 
 Page({
-  data: {
-  },
+  data: {},
   programId: null,
+  musicUrl: '',
   // url参数: programId
   onLoad(options) {
     const programId = options['programId']
@@ -18,19 +17,19 @@ Page({
     this.init()
   },
   async init() {
+    await this.fetchMusicUrl()
+    playerManager.playMusic(this.musicUrl)
   },
-  async fetchRadioDetail() {
+  async fetchMusicUrl() {
     try {
-      const response = await dao.getRadioDetail(this.radioId)
-      this.setData({
-        radioDetail: response.djRadio
-      })
+      const globalData = getApp().globalData
+      this.musicUrl = await dao.getMusicUrl(globalData.selectedProgram.mainSong.id, 128000)
     } catch (e) {
       console.error(e)
       wx.showToast({
         icon: 'none',
         duration: 2000,
-        title: '获取电台信息失败'
+        title: '获取音乐失败'
       })
     }
   },
