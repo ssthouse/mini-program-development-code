1ad8afdb54dd225a4e0d39619e575acbd07c9532
ssthouse <ssthouse@163.com>
2020-03-06 10:50:16 +0800

    chapter8: 实现radio播放页面进度控制部分

--- chapter_8/src/pages/radio/play/index.js
+++ chapter_8/src/pages/radio/play/index.js
@@ -1,9 +1,12 @@
 const regeneratorRuntime = require('../../../lib/runtime'); // eslint-disable-line
 const dao = require('../../../dao/index')
 const playerManager = require('../../../utils/player-manager')
+const PLAYER_EVENT_TYPE = playerManager.EVENT_TYPE
 
 Page({
-  data: {},
+  data: {
+    mainSong: null,
+  },
   programId: null,
   musicUrl: '',
   // url参数: programId
@@ -19,10 +22,14 @@ Page({
   async init() {
     await this.fetchMusicUrl()
     playerManager.playMusic(this.musicUrl)
+    this.initPlayerListener()
   },
   async fetchMusicUrl() {
     try {
       const globalData = getApp().globalData
+      this.setData({
+        mainSong: globalData.selectedProgram.mainSong
+      })
       this.musicUrl = await dao.getMusicUrl(globalData.selectedProgram.mainSong.id, 128000)
     } catch (e) {
       console.error(e)
@@ -33,4 +40,35 @@ Page({
       })
     }
   },
+  initPlayerListener() {
+    playerManager.registerEvent(PLAYER_EVENT_TYPE.ON_PLAY, this.onPlay)
+    playerManager.registerEvent(PLAYER_EVENT_TYPE.ON_PAUSE, this.onPause)
+    playerManager.registerEvent(PLAYER_EVENT_TYPE.ON_TIME_UPDATE, this.onPlayerTimeUpdate)
+  },
+  onPlay(){
+    const audioManager = wx.getBackgroundAudioManager()
+    this.setData({
+      isPlaying: true,
+      duration: audioManager.duration,
+      currentTime: audioManager.currentTime,
+      bufferedTime: audioManager.buffered
+    })
+  },
+  onPause(){
+    this.setData({
+      isPlaying: false
+    })
+  },
+  onPlayerTimeUpdate() {
+    const audioManager = wx.getBackgroundAudioManager()
+    this.setData({
+      duration: audioManager.duration,
+      currentTime: audioManager.currentTime,
+      bufferedTime: audioManager.buffered
+    })
+  },
+  onSeekMusic(e) {
+    const seekTime = e.detail.seekTime
+    playerManager.seekMusic(seekTime)
+  }
 })
