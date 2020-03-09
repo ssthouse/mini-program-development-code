6658a4f7ac0dde9418eb6c8681bc9ed309e1e60a
ssthouse <ssthouse@163.com>
2020-03-09 10:50:13 +0800

    chapter8: 调整recommend-tab文件结构

--- chapter_8/src/pages/index/index.js
+++ chapter_8/src/pages/index/index.js
@@ -4,39 +4,9 @@ const dao = require('../../dao/index')
 
 Page({
   data: {
-    loading: true,
-    playlists: [],
-    newSongList: [],
-    mvList: [],
-    djList: [],
     currentTabId: 'recommend'
   },
   onLoad() {
-    this.initData()
-  },
-  async initData() {
-    try {
-      const [playlists, newSongList, mvList, djList] = await Promise.all([
-        dao.getRecommendPlaylists(),
-        dao.getRecommendNewSong(),
-        dao.getRecommendMV(),
-        dao.getRecommendDJ()
-      ])
-      this.setData({
-        loading: false,
-        playlists: playlists.slice(0,6),
-        newSongList: newSongList.slice(0,6),
-        mvList: mvList.slice(0,6),
-        djList: djList.slice(0,6),
-      })
-      console.log('djList', djList)
-    } catch (e) {
-      wx.showToast({
-        icon: 'none',
-        duration: 1500,
-        title: '获取数据失败, 请稍后重试'
-      });
-    }
   },
   onChangeTab(e) {
     const tabId = e.detail
---
+++ chapter_8/src/pages/index/recommend-tab/index.js
@@ -0,0 +1,43 @@
+const regeneratorRuntime = require('../../../lib/runtime'); // eslint-disable-line
+const dao = require('../../../dao/index')
+
+Component({
+  data: {
+    loading: true,
+    playlists: [],
+    newSongList: [],
+    mvList: [],
+    djList: [],
+  },
+  lifetimes: {
+    attached() {
+      this.initData()
+    }
+  },
+  methods: {
+    async initData() {
+      try {
+        const [playlists, newSongList, mvList, djList] = await Promise.all([
+          dao.getRecommendPlaylists(),
+          dao.getRecommendNewSong(),
+          dao.getRecommendMV(),
+          dao.getRecommendDJ()
+        ])
+        this.setData({
+          loading: false,
+          playlists: playlists.slice(0,6),
+          newSongList: newSongList.slice(0,6),
+          mvList: mvList.slice(0,6),
+          djList: djList.slice(0,6),
+        })
+        console.log('djList', djList)
+      } catch (e) {
+        wx.showToast({
+          icon: 'none',
+          duration: 1500,
+          title: '获取数据失败, 请稍后重试'
+        });
+      }
+    },
+  },
+})
