e5bfbcb43edde34ec9126b912ff4474d83bee7af
ssthouse <ssthouse@163.com>
2020-03-21 16:07:48 +0800

    chapter8: add comment

--- chapter_8/src/components/comment-thread/index.js
+++ chapter_8/src/components/comment-thread/index.js
@@ -24,27 +24,28 @@ Component({
       this.fetchComments()
     }
   },
-  observers:{
-    'commentThreadId' :function () {
+  observers: {
+    'commentThreadId': function () {
       this.fetchComments()
     }
   },
   methods: {
     async fetchComments() {
-      if(!this.data.commentThreadId) return
+      if (!this.data.commentThreadId) return
       try {
-        console.log('')
         const response = await dao.getCommentList(this.data.commentThreadId, this.data.offset, PAGE_SIZE)
+        // hotComments字段仅在第一次拉取时存在,需要判空
         const hotComments = response.hotComments && response.hotComments.map(comment => {
-          return this.formatCommentTime(comment)
+          return this.formatCommentTime(comment) // 格式化评论时间
         })
         const newComments = response.comments.map(comment => {
-          return this.formatCommentTime(comment)
+          return this.formatCommentTime(comment) // 格式化评论时间
         })
         const newData = {}
         if (hotComments) newData.hotComments = hotComments
         const comments = this.data.comments.concat(newComments)
         newData.comments = comments
+        // 更新偏移量
         this.data.offset = comments.length
         this.hasMore = response.more
         this.setData(newData)
